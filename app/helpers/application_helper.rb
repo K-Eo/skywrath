afb330a862b5bc3a803e4442264e9d281502d407
Diego Lopez Garcia <aud0l@hotmail.com>
2017-10-19 23:31:32 -0500

    Add flash messages to devise views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def normalize_flash(key)
+    if key.nil?
+      return "info"
+    end
+
+    if key.is_a?(String)
+      key = key.to_sym
+    elsif !key.is_a?(Symbol)
+      return "info"
+    end
+
+    case key
+    when :notice
+      return "success"
+    when :success
+      return "success"
+    when :alert
+      return "error"
+    when :error
+      return "error"
+    end
+  end
 end
