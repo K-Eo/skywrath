b3afa47b737128ec0a64cf7405f78aad6d24c80d
Diego Lopez Garcia <aud0l@hotmail.com>
2017-10-20 19:07:00 -0500

    Add not found route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def after_inactive_sign_up_path_for(resource)
-    new_user_session_path
+  def route_not_found
+    render_404
+  end
+
+private
+
+  def render_404
+    respond_to do |format|
+      format.html do
+        render "layouts/404", status: "404"
+      end
+      format.any { head :not_found }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
+
+  get '*unmatched_route', to: 'application#route_not_found'
 end
