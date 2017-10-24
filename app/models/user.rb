c6566363e1612f9d8c7f944c137d0d811232759f
Diego Lopez Garcia <aud0l@hotmail.com>
2017-10-24 12:34:59 -0500

    Restore validatable for devise user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
-         :confirmable, :lockable
-
-  validates_presence_of :email, on: :create
-  validates_uniqueness_of :email, on: :create
-  validates_format_of :email, with: /\A[^@\s]+@[^@\s]+\z/, on: :create
-
-  validates_presence_of :password, on: [:create, :update_password]
-  validates_confirmation_of :password, on: [:create, :update_password]
-  validates_length_of :password, within: 6..128, on: [:create, :update_password]
+         :confirmable, :lockable, :validatable
 
   validates_presence_of :first_name, on: :update_profile
   validates_length_of :first_name, maximum: 50, on: :update_profile
@@ -37,16 +29,6 @@ class User < ApplicationRecord
   end
 
   def update_password(params)
-    current_password = params.delete(:current_password)
-
-    result = if valid_password?(current_password)
-      self.assign_attributes(params)
-      self.save(context: :update_password)
-    else
-      self.errors.add(:current_password, :invalid)
-      false
-    end
-
-    result
+    self.update_with_password(params)
   end
 end
