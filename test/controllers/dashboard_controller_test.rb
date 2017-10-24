db854765d741b3db54d913922cff0dce3604af7c
Diego Lopez Garcia <aud0l@hotmail.com>
2017-10-24 16:51:07 -0500

    Add tests

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def nuke
+    sign_in users(:eo)
+    get dashboard_path
+  end
+
+  test "gets show if logged in" do
+    nuke
+    assert_response :success
+    assert_select ".flash.messages"
+  end
+
+  test "renders default nav" do
+    nuke
+    assert_select "nav#default"
+  end
+
+  test "renders flash messages" do
+    nuke
+    assert_select ".flash.messages"
+  end
+
+  test "renders content wrapper" do
+    nuke
+    assert_select "#content-body"
+  end
+
+  test "redirects to new session if logged out" do
+    get dashboard_path
+    assert_redirected_to new_user_session_path
+  end
 end
--- test/controllers/settings/profile_controller_test.rb
+++ test/controllers/settings/profile_controller_test.rb
@@ -1,7 +1,52 @@
 require "test_helper"
 
 class Settings::ProfileControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  class Show < Settings::ProfileControllerTest
+    def nuke
+      @user = users(:eo)
+      sign_in @user
+      get settings_profile_path
+    end
+
+    test "responds with success" do
+      nuke
+      assert_response :success
+    end
+
+    test "renders default nav" do
+      nuke
+      assert_select "nav#default"
+    end
+
+    test "renders flash messages" do
+      nuke
+      assert_select ".flash.messages"
+    end
+
+    test "render content wraper" do
+      nuke
+      assert_select "#content-body"
+    end
+
+    test "renders settings sidebar" do
+      nuke
+      assert_select "#settings-sidebar" do
+        assert_select "a.item.active", "Perfil"
+      end
+    end
+
+    test "renders edit profile form" do
+      nuke
+      assert_select "#edit_user_#{@user.id}" do
+        assert_select "#user_name[value='#{@user.name}']"
+        assert_select "#user_phone[value='#{@user.phone}']"
+        assert_select "input[type=submit]"
+      end
+    end
+
+    test "redirects if not logged in" do
+      get settings_profile_path
+      assert_redirected_to new_user_session_path
+    end
+  end
 end
