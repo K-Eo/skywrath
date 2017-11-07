a72001f2082705b54126a331425f3fb2985fc2ae
Diego Lopez Garcia <aud0l@hotmail.com>
2017-11-06 22:09:26 -0600

    Add tests for profile controller

--- spec/controllers/users/profile_controller_spec.rb
+++ spec/controllers/users/profile_controller_spec.rb
@@ -1,5 +1,24 @@
 require "rails_helper"
 
 RSpec.describe Users::ProfileController, type: :controller do
+  let(:user) { create(:user) }
+  let(:action) { get :show }
+  subject { response }
 
+  before do
+    sign_in(user) if user
+    action
+  end
+
+  describe "GET #root" do
+    context "when logged in" do
+      it { is_expected.to have_http_status(:ok) }
+    end
+
+    context "when logged out" do
+      let(:user) { nil }
+
+      it { is_expected.to redirect_to(new_user_session_path) }
+    end
+  end
 end
