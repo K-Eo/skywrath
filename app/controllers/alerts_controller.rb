312fc01b7991f7c97631b3fa5977f9d9917a8152
Diego Lopez Garcia <aud0l@hotmail.com>
2017-11-21 14:49:21 -0600

    Clean alert creation
    - Remove create alert button in default nav.
    
    - Remove create action from alerts controller.
    
    - Remove rspec and spinach tests related to create alert with rails.

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -4,24 +4,4 @@ class AlertsController < ApplicationController
 
   def index
   end
-
-  def create
-    alert = Alert.new(author: current_user)
-
-    if alert.save
-      flash.now[:success] = "Alerta enviada."
-      NewAlertJob.perform_later(alert.id)
-    else
-      flash.now[:error] = "La alerta no se ha podido crear. Intente nuevamente."
-    end
-
-    load_alerts
-    render "index"
-  end
-
-private
-
-  def load_alerts
-    @alerts = Alert.newest.includes(:author).page(params[:page]).per(24)
-  end
 end
