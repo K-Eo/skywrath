85d087aabed1700d335cc31aca694f6922c5f55f
Diego Lopez Garcia <aud0l@hotmail.com>
2017-12-19 16:32:46 -0600

    Add lat and lng
    - Add lat and lng to users model
    
    - Add users to seeds file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128231608) do
+ActiveRecord::Schema.define(version: 20171219221326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 20171128231608) do
     t.string "phone"
     t.string "name"
     t.string "access_token"
+    t.float "lat"
+    t.float "lng"
     t.index ["access_token"], name: "index_users_on_access_token", unique: true
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+users = [
+  ["David Eyre", "david@skywrath.com", "8772193", 17.262889, -97.6831033],
+  ["Mia Volkova", "mia@skywrath.com", "2048666", 17.2743233, -97.6897572],
+  ["Sherri Jones", "sherri@skywrath.com", "9692707", 17.268371, -97.6765177],
+  ["Tom Hamada", "tom@skywrath.com", "8503886", 17.271895, -97.6781167]
+]
+
+users.each do |name, email, phone, lat, lng|
+  user = User.create!(email: email,
+                      lat: lat,
+                      lng: lng,
+                      name: name,
+                      password: "password",
+                      phone: phone)
+  user.confirm
+end
